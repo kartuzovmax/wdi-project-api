22648eda24b559c377a9ece46f7cdaebe4c58499
loyd0 <samloyd@hotmail.co.uk>
2017-05-30 13:56:50 +0100

    really cool :poop: jsut went down, we added an authentications controller and edited the routes

--- reddit-clone/config/routes.rb
+++ reddit-clone/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     resources :comments
     resources :posts
     resources :users
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
   end
 end
