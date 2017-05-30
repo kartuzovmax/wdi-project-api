62d6d66f3d5e8ce1bc26d29f558db9ef8bcd2a24
loyd0 <samloyd@hotmail.co.uk>
2017-05-30 13:54:13 +0100

    maxeyyyyyy :poop:

--- reddit-clone/app/models/user.rb
+++ reddit-clone/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
   has_many :comments
+#Validations
+  validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: true
 end
